d2216374a74d6331706606d0f72cada58ef3946b
League and Chill <enzo.marchetti97@gmail.com>
2024-11-30 16:17:07 +0100

    Added design district image text slideshow

--- src/scrollHandler.ts
+++ src/scrollHandler.ts
@@ -1,5 +1,5 @@
 import { ScrollableSlideShow } from "./ScrollableSlideShow";
-import { clamp, entries } from "lodash";
+import { clamp, difference, entries } from "lodash";
  
  //Triple slideshow
   var lastScrollY = 0;
@@ -9,11 +9,21 @@ import { clamp, entries } from "lodash";
   var slideShow2 = new ScrollableSlideShow("#firstSlideShowSecondElement", 1);
   var slideShow3 = new ScrollableSlideShow("#firstSlideShowThirdElement", -1);
 
+  var SecondslideShow1 = new ScrollableSlideShow("#SecondSlideShowFirstElement", -1);
+  var SecondslideShow2 = new ScrollableSlideShow("#SecondSlideShowSecondElement", 1);
+  var SecondslideShow3 = new ScrollableSlideShow("#SecondSlideShowThirdElement", -1);
+
   window.addEventListener('scroll', () => {
     lastScrollY = currentScrollY;
     currentScrollY = window.scrollY;
     scrollDelta = currentScrollY - lastScrollY;
-    slideShow1.scrollElement(clamp(scrollDelta, -1, 1), scrollDelta);
-    slideShow2.scrollElement(clamp(scrollDelta, -1, 1), scrollDelta);
-    slideShow3.scrollElement(clamp(scrollDelta, -1, 1), scrollDelta);
+    var direction = clamp(scrollDelta, -1, 1);
+
+    slideShow1.scrollElement(direction, scrollDelta);
+    slideShow2.scrollElement(direction, scrollDelta);
+    slideShow3.scrollElement(direction, scrollDelta);
+
+    SecondslideShow1.scrollElement(direction, scrollDelta);
+    SecondslideShow2.scrollElement(direction, scrollDelta);
+    SecondslideShow3.scrollElement(direction, scrollDelta);
   })
